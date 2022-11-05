fb10957d7be5fc98802e31243757f4834595d3f8
Fabio Freitas <fabio1_freitas@hotmail.com>
2022-11-05 19:10:04 -0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'temp.fabiofreitas.com.br']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
